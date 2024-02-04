31418bc0a15aff0e697978125dd7a7b2eeeb8a99
Meowtomata <its.anatoliy@gmail.com>
2024-02-04 08:42:20 -0500

    Implement 04_removeFromArray
    Practiced using .filter(), a lambda function,
    .includes, and multiple args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...arg) {
+    // return array with elements that do not include any elements from args
+    return array.filter((x) => !arg.includes(x));
+    
 };
 
 // Do not edit below this line
