14545253d7e3604717cd8c48c4e97f10a6bef5ac
Meowtomata <its.anatoliy@gmail.com>
2024-02-04 08:54:29 -0500

    Implement 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(lowerInt, upperInt) {
 
+    if (lowerInt < 0 || upperInt < 0) return "ERROR";
+    if (typeof (lowerInt + upperInt) != "number") return "ERROR";
+    higher = Math.max(lowerInt, upperInt);
+    lower = Math.min(lowerInt, upperInt);
+
+    let sum = 0;
+    for (; lower <= higher; lower++) {
+        sum += lower;
+    }
+    return sum;
 };
 
 // Do not edit below this line
