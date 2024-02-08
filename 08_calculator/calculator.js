fe2613f141f159940cd17eb18eca3b08ad9a3a0d
Meowtomata <its.anatoliy@gmail.com>
2024-02-07 21:15:52 -0500

    Implement 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(...args) {
+	return args.reduce((a,b) => {return a + b},0);
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args.reduce((a,b) => {return -a - b},0);
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	return args.reduce((a,b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((a,b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  total = 1;
+  for (let i = 0; i < b; i++) {
+    total *= a;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a < 1) {
+    return 1;
+  } else {
+    return a * factorial(a-1);
+  }
 };
 
 // Do not edit below this line
